ed51a930254f875ff5c5938271ec27350f014d18
brandonvellien <brandon.vellien@gmail.com>
2024-08-07 00:31:35 +0200

    fetch url

--- backend/Routes/recipes.js
+++ backend/Routes/recipes.js
@@ -6,5 +6,5 @@ const axios = require("axios");
 const apiKey = process.env.KITCHENACT_API_KEY;
 // Route pour obtenir les recettes
 router.get("/home", async (req, res) => {
-  const url = `https://api.spoonacular.com/recipes/complexSearch?query=salad&sort=healthiness&maxCalories=700&minProtein=20&number=20&apiKey=${apiKey}`;
+  const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&sort=healthiness&maxCalories=700&minProtein=20&number=20&apiKey=${apiKey}`;
 });
