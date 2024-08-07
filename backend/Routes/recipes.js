3fb09da8c22fe690c89fd69f8a355c1b296f6476
brandonvellien <brandon.vellien@gmail.com>
2024-08-07 16:32:40 +0200

    add search result routes and filters

--- backend/Routes/recipes.js
+++ backend/Routes/recipes.js
@@ -4,7 +4,44 @@ const router = express.Router();
 const axios = require("axios");
 
 const apiKey = process.env.KITCHENACT_API_KEY;
-// Route pour obtenir les recettes
+
+// Route obtenir des recettes random
 router.get("/home", async (req, res) => {
-  const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&sort=healthiness&maxCalories=700&minProtein=20&number=20&apiKey=${apiKey}`;
+  const url = `https://api.spoonacular.com/recipes/random?number=15&apiKey=${apiKey}`;
+  try {
+    const response = await axios.get(url);
+    res.json(response.data.recipes);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({
+      error: "An error has occurred",
+    });
+  }
+});
+
+// Route pour obtenir les recettes avec recherhce
+router.get("/home/searchresults", async (req, res) => {
+  let url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=20&apiKey=${apiKey}`;
+
+  if (sort) {
+    url = url + `&sort=${sort}`;
+  }
+  if (cuisine) {
+    url = url + `&cuisine=${cuisine}`;
+  }
+  if (diet) {
+    url = url + `&diet=${diet}`;
+  }
+  if (type) {
+    url = url + `&type=${type}`;
+  }
+  try {
+    const response = await axios.get(url);
+    res.json(response.data.results);
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).json({
+      error: "An error has occurred",
+    });
+  }
 });
